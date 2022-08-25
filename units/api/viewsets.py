256e0ebd85b51fcebc5879f2cb0bcf9489d5c74d
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2022-08-25 14:14:32 -0300

    Logic Update

--- units/api/viewsets.py
+++ units/api/viewsets.py
@@ -14,16 +14,16 @@ class UnitsViewSet(viewsets.ModelViewSet):
     def generate_error_message(self, message):
         return {
             'success' : False,
-            'message' : 'paramters not found'
+            'message' : message
         }
 
     def calculate_membership_fee(self, rent_amount, rent_period, organisation_unit):
         if(rent_period == 'week'):
             if(rent_amount < 25 or rent_amount > 2000):
-                self.generate_error_message("Rent amount's outiside the range")
+                return -1
         if(rent_period == 'month'):
             if(rent_amount < 110 or rent_amount > 8660):
-                self.generate_error_message("Rent amount's outiside the range")
+                return -1
         if(organisation_unit.get('has_fixed_membership_fee')):
             return organisation_unit.get('fixed_membership_fee_amount')
         calc = int(rent_amount * 0.2)
@@ -33,15 +33,17 @@ class UnitsViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         param = kwargs.get('pk')
-        param = param.split(',')
+        param = param.split(':')
         id_unit_param = param[0]
-        if (param[1] == None or param[2] == None):
-            return Response(self.generate_error_message('paramters not found'))
+        if (len(param) != 3):
+            return Response(self.generate_error_message('Paramters not found'))
         rent_amount = int(param[1])
         rent_period = param[2]
         unit = models.Units.objects.filter(id_units=id_unit_param)
         serializer = UnitsSerializer(unit, many=True)
         membership_fee = self.calculate_membership_fee(rent_amount, rent_period, serializer.data[0])
+        if(membership_fee == -1):
+            return Response(self.generate_error_message("Rent amount's outiside the range"))
         return Response({
             'success' : True,
             'rent amount' : rent_amount,
